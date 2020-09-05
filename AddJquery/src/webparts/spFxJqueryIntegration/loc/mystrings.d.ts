d69ca29487e6f7c3ef161d747f7d2a561588631b
Jenkins NS <jenkinsns@gmail.com>
2020-09-05 23:34:41 +0530

    added

---
+++ AddJquery/src/webparts/spFxJqueryIntegration/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxJqueryIntegrationWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxJqueryIntegrationWebPartStrings' {
+  const strings: ISpFxJqueryIntegrationWebPartStrings;
+  export = strings;
+}
